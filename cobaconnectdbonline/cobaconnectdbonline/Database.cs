60df678451ac656563082fc8a29eff581b2cc423
bilaismi5 <nabilaismi456@gmail.com>
2026-01-01 10:44:57 +0700

    login regis dan crud kabupaten_kota

---
+++ cobaconnectdbonline/cobaconnectdbonline/Database.cs
@@ -0,0 +1,30 @@
+﻿using cobaconnectdbonline.Models;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cobaconnectdbonline
+{
+    public class Database
+    {
+        private MongoClient client;
+        private IMongoDatabase database;
+
+        public Database()
+        {
+            client = new MongoClient(
+            "mongodb+srv://isi yg kalian");
+
+            database = client.GetDatabase("db_sampah");
+        }
+
+        public IMongoCollection<User> Users =>
+            database.GetCollection<User>("users");
+
+        public IMongoCollection<KabupatenKota> Kabupaten =>
+            database.GetCollection<KabupatenKota>("kabupaten_kota");
+    }
+}
